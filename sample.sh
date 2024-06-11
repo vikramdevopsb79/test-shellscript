897ffb3a2ca82ac29c985ef04d238b566be9367f
sai <kuppalasaivikram@gmail.com>
2024-06-11 09:53:57 +0530

    creating remote backend state file for multi env

---
+++ sample.sh
@@ -0,0 +1,24 @@
+#Write a script to print number of "S" in Mississippi
+
+#word="Mississippi"
+#for i in word; do
+
+# Write a script to print only errors from a remote log4
+
+  cat remotefilecontent | grep error &>>/tmp/logfilecapature
+
+ # How will you debug the shell script?
+ stat() {
+   if [ $1 -eq 0 ]; then
+   echo success
+   else
+     echo failure
+    fi
+ }
+
+  #Write a simple shell script to list all processes
+
+  ps -ef
+
+
+
